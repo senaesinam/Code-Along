d4eb885648abb3eacfcf72befd42fe5089c785bc
EmmanuelFenyi <phylliswaltersandoh@email.com>
2022-03-25 15:45:52 +0000

    react-component-3

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+// import writers from './writers';
+import { ProfileCard } from './ProfileCard';
+import {useEffect, useState} from "react";
+
+function App() {
+const [writers, setWriters] = useState([]);
+
+  useEffect(() => {
+    const getWriters = async () => {
+      const response = await fetch ("/writers.json");
+      const data = await response.json();
+      setWriters(data);
+    };
+    getWriters();
+  },[]);
+
+  return (
+    <div>
+      <h1>WRITER PROFILES</h1>
+      <div className="container">
+        {writers.map(writer => (
+          <ProfileCard writer={writer}/>
+        ))}
+      </div>
+      
+      
+
+      
+    </div>
+  );
+}
+
+export default App;
