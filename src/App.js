c76a7fd469935d806191eeaeae3b4d356b696a74
EmmanuelFenyi <phylliswaltersandoh@email.com>
2022-03-25 15:10:09 +0000

    react component

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import writers from './writers';
+import { ProfileCard } from './ProfileCard';
+
+function App() {
+  return (
+    <div>
+      <h1>WRITER PROFILES</h1>
+      <div className="container">
+        {writers.map(writer => (
+          <ProfileCard writer={writer}/>
+        ))}
+      </div>
+      
+      
+
+      
+    </div>
+  );
+}
+
+export default App;
