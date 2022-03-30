40543ccd388b8c7819be84e803001e5140b6de12
Ellen Nenyonyoge <senaesinam1@gmail.com>
2022-03-30 17:25:05 +0000

    react component 7

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import useLocalStorage from "./hooks/useLocalStorage";
+import ProfileForm from "./components/ProfileForm";
+import ProfileCard from "./components/ProfileCard";
+
+
+function App() {
+  const [profiles, setProfiles] = useLocalStorage("profiles", []);
+
+  const updateProfiles = (profile) => {
+    let arr = profiles;
+    arr.push(profile);
+    setProfiles([...arr]);
+  };
+
+  return (
+    <div className="app">
+      <h1> Profile Maker </h1>
+      <div>
+        <ProfileForm submit={updateProfiles} />
+        <hr />
+        <div className="list">
+          {profiles.map((person, index) => (
+            <ProfileCard key={index} writer={person} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+const useLocalStorage = (key, initialValue) => {
+const [value, setValue] = useState(() => {
+
+    if (typeof window === undefined) {
+        return initialValue;
+    }
+    
+    try {
+        const data = window.localStorage.getItem(key);
+        return data? JSON.parse(data) : initialValue;
+    
+    } catch (error) {
+        console.log(error);
+        return initialValue;
+    }
+});
+
+const setLocalStorage = (value) => {
+    try {
+        setValue(value);
+
+        if (typeof window !== undefined) {
+            window.localStorage.setItem(key, JSON.stringify(value));
+        }
+    } catch (error) {
+        console.log(error);
+        
+    }
+};
+  return [value, setLocalStorage];
+}
+
+export default useLocalStorage;
