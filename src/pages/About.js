9c2ef8495d14b2f500ca04cccbcfbfcff6199865
Ellen Nenyonyoge <senaesinam1@gmail.com>
2022-04-04 17:38:39 +0000

    react-component-8

---
+++ src/pages/About.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section>
+        <h1>What we offer</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit tempora quas et perferendis sequi optio vitae quia ex laborum. Iste dolores, minima repellendus ipsum recusandae architecto consequatur possimus repudiandae aliquam.
+        Exercitationem, magni ab? Nisi itaque quod quo excepturi voluptatibus nam possimus, nihil laborum delectus esse, minima recusandae quasi, eius eaque earum quibusdam corrupti! Tempora voluptate quo ipsa mollitia minima aliquid?
+        Iste, expedita, commodi dignissimos accusamus totam reiciendis aspernatur magni minima qui suscipit temporibus exercitationem impedit culpa, hic id corporis. Neque odio optio eius deleniti perferendis ipsam error dignissimos aperiam placeat.
+        </p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit tempora quas et perferendis sequi optio vitae quia ex laborum. Iste dolores, minima repellendus ipsum recusandae architecto consequatur possimus repudiandae aliquam.
+        Exercitationem, magni ab? Nisi itaque quod quo excepturi voluptatibus nam possimus, nihil laborum delectus esse, minima recusandae quasi, eius eaque earum quibusdam corrupti! Tempora voluptate quo ipsa mollitia minima aliquid?
+        Iste, expedita, commodi dignissimos accusamus totam reiciendis aspernatur magni minima qui suscipit temporibus exercitationem impedit culpa, hic id corporis. Neque odio optio eius deleniti perferendis ipsam error dignissimos aperiam placeat.
+        </p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit tempora quas et perferendis sequi optio vitae quia ex laborum. Iste dolores, minima repellendus ipsum recusandae architecto consequatur possimus repudiandae aliquam.
+        Exercitationem, magni ab? Nisi itaque quod quo excepturi voluptatibus nam possimus, nihil laborum delectus esse, minima recusandae quasi, eius eaque earum quibusdam corrupti! Tempora voluptate quo ipsa mollitia minima aliquid?
+        Iste, expedita, commodi dignissimos accusamus totam reiciendis aspernatur magni minima qui suscipit temporibus exercitationem impedit culpa, hic id corporis. Neque odio optio eius deleniti perferendis ipsam error dignissimos aperiam placeat.
+        </p>
+    </section>
+  )
+}
+
+export default About
---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section>
+        <h1>Welcome to our Home Page</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit tempora quas et perferendis sequi optio vitae quia ex laborum. Iste dolores, minima repellendus ipsum recusandae architecto consequatur possimus repudiandae aliquam.
+        Exercitationem, magni ab? Nisi itaque quod quo excepturi voluptatibus nam possimus, nihil laborum delectus esse, minima recusandae quasi, eius eaque earum quibusdam corrupti! Tempora voluptate quo ipsa mollitia minima aliquid?
+        Iste, expedita, commodi dignissimos accusamus totam reiciendis aspernatur magni minima qui suscipit temporibus exercitationem impedit culpa, hic id corporis. Neque odio optio eius deleniti perferendis ipsam error dignissimos aperiam placeat.
+        </p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit tempora quas et perferendis sequi optio vitae quia ex laborum. Iste dolores, minima repellendus ipsum recusandae architecto consequatur possimus repudiandae aliquam.
+        Exercitationem, magni ab? Nisi itaque quod quo excepturi voluptatibus nam possimus, nihil laborum delectus esse, minima recusandae quasi, eius eaque earum quibusdam corrupti! Tempora voluptate quo ipsa mollitia minima aliquid?
+        Iste, expedita, commodi dignissimos accusamus totam reiciendis aspernatur magni minima qui suscipit temporibus exercitationem impedit culpa, hic id corporis. Neque odio optio eius deleniti perferendis ipsam error dignissimos aperiam placeat.
+        </p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit tempora quas et perferendis sequi optio vitae quia ex laborum. Iste dolores, minima repellendus ipsum recusandae architecto consequatur possimus repudiandae aliquam.
+        Exercitationem, magni ab? Nisi itaque quod quo excepturi voluptatibus nam possimus, nihil laborum delectus esse, minima recusandae quasi, eius eaque earum quibusdam corrupti! Tempora voluptate quo ipsa mollitia minima aliquid?
+        Iste, expedita, commodi dignissimos accusamus totam reiciendis aspernatur magni minima qui suscipit temporibus exercitationem impedit culpa, hic id corporis. Neque odio optio eius deleniti perferendis ipsam error dignissimos aperiam placeat.
+        </p>
+    </section>
+  )
+}
+
+export default Home
